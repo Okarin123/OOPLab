6f9311f394fbf38be20684749690ed21b5548463
Okarin123 <42962897+Okarin123@users.noreply.github.com>
2018-09-07 10:56:48 +0530

    added a finally to try catch block!

--- labclass8/ex8#1.java
+++ labclass8/ex8#1.java
@@ -5,13 +5,16 @@ class ex1 {
 		try {
 
 			System.out.println("Before throwing an exception."); 
-			throw new Exception(); 
+			throw new Exception("Passing a String Argument."); 
 		}
 
 		catch (Exception e) {
-			System.out.println("Exception caught."); 
+			System.out.println("Exception along with its string message: " + e); 
+		} 
+		
+		finally { 
+			System.out.println("Program to pass string argument to Exception complete.");
 		}
-
 	}
 } 
 
